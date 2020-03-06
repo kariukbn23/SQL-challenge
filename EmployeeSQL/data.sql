04f90cd89e9ce17f5d0f801a11fa6b00aa872813
kariukbn23 <54543371+kariukbn23@users.noreply.github.com>
2020-03-05 20:11:47 -0600

    Update 6

--- EmployeeSQL/data.sql
+++ EmployeeSQL/data.sql
@@ -1,2 +1,33 @@
-# a comment 
+# SQL Query Code for Table 1 (Departments) that I created prior to importing the data 
+
+CREATE TABLE Departments (
+    dept_no     VARCHAR NOT NULL,
+    dept_name   VARCHAR NOT NULL,
+    PRIMARY KEY (dept_no)
+    
+);
+
+SELECT * FROM Departments; 
+
+# Before creating second table for employees I deleted the Departments table created above, for space purposes. 
+
+DROP TABLE Departments;
+
+# The next table created below was with the employee information just because my second primary key that will be used to link to 2 of the other csv is originally contained in this table. 
+
+CREATE TABLE Employees (
+    emp_no INT NOT NULL,
+    birth_date  DATE NOT NULL,
+    first_name  VARCHAR NOT NULL,
+    last_name   VARCHAR NOT NULL,
+    gender      VARCHAR NOT NULL,    
+    hire_date   DATE NOT NULL,
+    PRIMARY KEY (emp_no)
+);
+
+SELECT * FROM Employees; 
+
+# Before creating third table for Dept_emp I deleted the Employees table created above, for space purposes.
+
+DROP TABLE Employees;
 
